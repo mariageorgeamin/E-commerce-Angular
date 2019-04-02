b072e2f1911565ddbbbb1b20f95b330b891d76cf
mariageorgeamin <46920076+mariageorgeamin@users.noreply.github.com>
2019-04-02 19:39:21 +0200

    Routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,27 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { AppComponent } from "./components/app/app.component";
+import { HomeComponent } from "./components/home/home.component";
+import { LoginComponent } from "./components/login/login.component";
+import { CartComponent } from "./components/cart/cart.component";
+import { SignupComponent } from "./components/signup/signup.component";
+import { WishlistComponent } from "./components/wishlist/wishlist.component";
+import { SingleproductComponent } from "./components/singleproduct/singleproduct.component";
+import { NotfoundComponent } from "./components/notfound/notfound.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: AppComponent },
+  { path: "/home", component: HomeComponent },
+  { path: "/login", component: LoginComponent },
+  { path: "/cart", component: CartComponent },
+  { path: "/signup", component: SignupComponent },
+  { path: "/wishlist", component: WishlistComponent },
+  { path: "/singleproduct", component: SingleproductComponent },
+  { path: "**", component: NotfoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
