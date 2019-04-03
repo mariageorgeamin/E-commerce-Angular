edf5d711918fe1d25a798faf5eb29f906a83dc49
mariageorgeamin <46920076+mariageorgeamin@users.noreply.github.com>
2019-04-03 21:11:07 +0200

    adding bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,37 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
-
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./components/app/app.component";
 import { CartComponent } from "./components/cart/cart.component";
-import { HomeComponent } from './components/home/home.component';
-import { LoginComponent } from './components/login/login.component';
-import { NotfoundComponent } from './components/notfound/notfound.component';
-import { SignupComponent } from './components/signup/signup.component';
-import { SingleproductComponent } from './components/singleproduct/singleproduct.component';
-import { WishlistComponent } from './components/wishlist/wishlist.component';
+import { HomeComponent } from "./components/home/home.component";
+import { LoginComponent } from "./components/login/login.component";
+import { NotfoundComponent } from "./components/notfound/notfound.component";
+import { SignupComponent } from "./components/signup/signup.component";
+import { SingleproductComponent } from "./components/singleproduct/singleproduct.component";
+import { WishlistComponent } from "./components/wishlist/wishlist.component";
+import { HeaderComponent } from "./components/header/header.component";
+import { ReactiveFormsModule, FormsModule } from "@angular/forms";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 
 @NgModule({
-  declarations: [AppComponent, CartComponent, HomeComponent, LoginComponent, NotfoundComponent, SignupComponent, SingleproductComponent, WishlistComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [
+    AppComponent,
+    CartComponent,
+    HomeComponent,
+    LoginComponent,
+    NotfoundComponent,
+    SignupComponent,
+    SingleproductComponent,
+    WishlistComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule,
+    ReactiveFormsModule,
+    FormsModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
