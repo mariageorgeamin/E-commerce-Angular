74f0ced4b36b4422dd1ced94069d5d4e3477b9df
mariageorgeamin <46920076+mariageorgeamin@users.noreply.github.com>
2019-04-03 21:11:56 +0200

    signup form

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { MustMatch } from "src/app/_helpers/must-match.validators";
+
+// import custom validator to validate that password and confirm password fields match
 
 @Component({
-  selector: 'app-signup',
-  templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.scss']
+  selector: "app-signup",
+  templateUrl: "./signup.component.html",
+  styleUrls: ["./signup.component.scss"]
 })
 export class SignupComponent implements OnInit {
-
-  constructor() { }
+  registerForm: FormGroup;
+  submitted = false;
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit() {
+    this.registerForm = this.formBuilder.group(
+      {
+        firstName: ["", Validators.required],
+        email: ["", [Validators.required, Validators.email]],
+        password: ["", [Validators.required, Validators.minLength(6)]],
+        confirmPassword: ["", Validators.required]
+      },
+      {
+        validator: MustMatch("password", "confirmPassword")
+      }
+    );
   }
 
+  // convenience getter for easy access to form fields
+  get f() {
+    return this.registerForm.controls;
+  }
+
+  onSubmit() {
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.registerForm.invalid) {
+      return;
+    }
+
+    alert("SUCCESS!! :-)\n\n" + JSON.stringify(this.registerForm.value));
+  }
 }
