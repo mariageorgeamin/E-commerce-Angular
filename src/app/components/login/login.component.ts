a80c17afcd521419317cb48cc414985551dca4e5
mariageorgeamin <46920076+mariageorgeamin@users.noreply.github.com>
2019-04-03 21:12:40 +0200

    login form

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.scss"]
 })
 export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+  submitted = false;
+  constructor(private router: Router, private formBuilder: FormBuilder) {}
 
-  constructor() { }
-
-  ngOnInit() {
+  public navigateTo(path: string): void {
+    this.router.navigate([path]);
   }
 
+  ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      email: ["", [Validators.required, Validators.email]],
+      password: ["", [Validators.required, Validators.minLength(6)]]
+    });
+  }
+  // convenience getter for easy access to form fields
+  get f() {
+    return this.loginForm.controls;
+  }
+
+  onSubmit() {
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.loginForm.invalid) {
+      return;
+    }
+
+    this.router.navigateByUrl("home");
+    // alert("SUCCESS!! :-)\n\n" + JSON.stringify(this.loginForm.value));
+  }
 }
