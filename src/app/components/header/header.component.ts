01811b25d9dd215b5ecc302944c4ebb1c8f419de
mariageorgeamin <46920076+mariageorgeamin@users.noreply.github.com>
2019-04-05 09:46:49 +0200

    showing name of user in navbar

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Router, RouterLink } from "@angular/router";
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  selector: "app-header",
+  templateUrl: "./header.component.html",
+  styleUrls: ["./header.component.scss"]
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  public name;
+  constructor(private router: Router) {
+    this.name = localStorage.getItem("name");
+    console.log(this.name);
   }
 
+  ngOnInit() {}
+
+  logout() {
+    localStorage.removeItem("name");
+    this.router.navigate([""]);
+  }
 }
