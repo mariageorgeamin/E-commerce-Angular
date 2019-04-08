2d4c5f2124e34ed6c1fb80a647ab8cf12ca8f591
mariageorgeamin <mariageorgeamin@yahoo.com>
2019-04-08 23:33:26 +0200

    adding header cart and wishlist

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from "@angular/core";
-import { Router, RouterLink } from "@angular/router";
+import { Router, RouterLink, ActivatedRoute } from "@angular/router";
+import { Item } from "src/app/item";
+import { ProductService } from "src/app/services/product.service";
+import { StreamService } from "src/app/services/stream.service";
 
 @Component({
   selector: "app-header",
@@ -8,15 +11,29 @@ import { Router, RouterLink } from "@angular/router";
 })
 export class HeaderComponent implements OnInit {
   public name;
-  constructor(private router: Router) {
-    this.name = localStorage.getItem("name");
+  public cartCount;
+  public wishListCount;
+  private items: Item[] = [];
+  private total: number = 0;
+  private totalQty: number = 0;
+  constructor(
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+    private productService: ProductService,
+    private stream: StreamService
+  ) {
+    this.stream.getName().subscribe(res => (this.name = res));
+    this.stream.getcartCount().subscribe(res => (this.cartCount = res));
+    this.stream.getwishListCount().subscribe(res => (this.wishListCount = res));
     console.log(this.name);
   }
 
   ngOnInit() {}
-
   logout() {
     localStorage.removeItem("name");
+    localStorage.removeItem("cart");
+    localStorage.removeItem("wishlist");
+    this.stream.setName(localStorage.getItem("name"));
     this.router.navigate([""]);
   }
 }
